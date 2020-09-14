c2c0fa3d5c91b4e1b6069036011e4f92cc494949
manas-anarov <you@example.com>
2020-09-14 13:50:41 +0600

    8_lesson

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,25 @@
-console.log('test')
+// let greet = () =>{
+//     console.log('hello world');
+// }
+
+
+let greet: Function;
+
+
+greet = () => {
+    console.log('hello again');
+}
+
+const add = (a: number, b: number, c: number | string = 10): void => {
+    console.log(a  + b);
+    console.log(c)
+
+}
+
+add(5, 10, '20');
+
+const minus = (a: number, b: number): number => {
+    return a + b;
+}
+
+let result = minus(10, 7);
