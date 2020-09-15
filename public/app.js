2c07a5448a25a859308e200b1758673a2ced3faf
manas-anarov <you@example.com>
2020-09-15 15:28:50 +0600

    11_lesson

---
+++ public/app.js
@@ -0,0 +1,18 @@
+"use strict";
+// const anchor = document.querySelector('a')!;
+// if (anchor){
+//     console.log(anchor.href);
+// }
+// console.log(anchor.href);
+console.log("sas");
+// const form = document.querySelector('form')!;
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var tofrom = document.querySelector('#tofrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.value);
+});
+console.log(type);
