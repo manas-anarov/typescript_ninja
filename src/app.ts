d78b6f89bfb49ad7164708ff761981f405663a66
manas-anarov <you@example.com>
2020-09-20 19:58:59 +0600

    14

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,5 @@
+import {Invoice} from './classes/Invoice.js'
 
-class Invoice{
-    // readonly client: string;
-    // private details: string;
-    // public amount: number;
-
-    constructor(
-        readonly client:string,
-        private details: string,
-        public amount: number,
-    ){
-        // this.client = c;
-        // this.details = b,
-        // this.amount = a;
-    }
-
-    format(){
-        return `${this.client} owes f${this.amount} for ${this.details}`;
-    }
-}
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
